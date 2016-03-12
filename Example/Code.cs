46bec060777dd551ace7411742e5ee93701e0d61
Matt Klein <mattklein999@gmail.com>
2016-03-12 15:06:08 -0700

    Added example code

--- Example/Code.cs
+++ Example/Code.cs
@@ -1,48 +1,4 @@
-[![Build status](https://ci.appveyor.com/api/projects/status/3sp5o4fuhspo616s/branch/master?svg=true)](https://ci.appveyor.com/project/mattklein999/liketype/branch/master)
-# LikeType
-
-Provides 'typedef' like behavior to simple C# classes.
-
-The syntax to use is:
-
-    class MyClass : LikeType<BackingType>
-    {
-        public MyClass(BackingType value) : base(value) { }
-    }
-
-Example:
-
-    class CustomerId : LikeType<string>
-    {
-        public CustomerId(string id) : base(id) { }
-    }
-
-    
-## Behavior
-
-Here is how the type will behave:
-
-    void ShowTypeBehavior()
-    {
-        var customerId = new CustomerId("cust-001"); // create instance with given backing value
-        string custIdValue = customerId; // implicit cast from class to backing type, sets 'custIdValue' to "cust-001"
-        
-        var otherCustomerId = new CustomerId("cust-002");
-        var areEqual = customerId == otherCustomerId; // false
-        var areNotEqual = customerId != otherCustomerId; // true
-        var areEqualUsingMethod = customerId.Equals(otherCustomerId); // false
-        
-        var customerIdCopy = new CustomerId("cust-001"); // create separate instance with same backing value
-        var isCopyEqual = customerId == customerIdCopy; // true. Instances are considered equal if their backing values are equal.
-    }
-
-The backing field is immutable and your class should probably also be immutable. If you add additional properties to you class, then make sure to override the `.Equals()` method and `.GetHashCode()` method to first delegate to the base class.
-
-## Example
-
-Lets say that you have an order printing service that takes in a `CustomerId` and prints `Order`s for that customer. You want to test the interaction between the printing sevice, the order service, and the printer.
-
-    using System.Collections.Generic;
+﻿    using System.Collections.Generic;
     using System.Linq;
     using Kleinware.LikeType;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
